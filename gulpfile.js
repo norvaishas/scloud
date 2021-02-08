7998421691780f81ee6d83c5b08706b0eb0f688c
Sergei Norvaishas <snorvaishas@yandex.ru>
2021-02-09 02:35:21 +0300

    feat: add header

--- gulpfile.js
+++ gulpfile.js
@@ -20,8 +20,7 @@ gulp.task('server', function() {
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(sass().on('error', sass.logError))
         // .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
